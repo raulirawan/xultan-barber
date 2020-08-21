4e495032db83a564c8aeb9415bc02fc7e48aa611
Raul Irawan <raulirawan33@gmail.com>
2020-08-21 18:01:25 +0700

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+//Porfolio isotope and filter
+$(window).on("load", function () {
+  //   var portfolioIsotope = $(".gallery-container").isotope({
+  //     itemSelector: ".gallery-item",
+  //     layoutMode: "fitRows",
+  //   });
+
+  // Initiate venobox (lightbox feature used in portofilo)
+  $(document).ready(function () {
+    $(".venobox").venobox();
+  });
+});
