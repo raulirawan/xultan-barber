052cf9ebeeb5dbb6bc0ced4f4779b1eb5e466b41
Raul Irawan <raulirawan33@gmail.com>
2020-08-24 11:48:48 +0700

    menambahkan animasi scrolling

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,15 @@ $(window).on("load", function () {
     $(".venobox").venobox();
   });
 });
+
+$(".page-scroll").on("click", function (e) {
+  var tujuan = $(this).attr("href");
+
+  var elemenTujuan = $(tujuan);
+
+  $("html , body").animate({
+    scrollTop: elemenTujuan.offset().top - 50,
+  });
+
+  e.preventDefault();
+});
